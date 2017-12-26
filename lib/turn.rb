c1bda129bd5ff24d3cc9a212f3d2e6fc59ca43a6
Ingrid Wong <yw340@georgetown.edu>
2017-12-26 05:39:25 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,12 @@
+def display_board(board)
+
+end
+
+def valid_move?(board, index)
+ return index.between(0,8) $$ !position_taken
+end
+
+def position_taken?(board, index)
+  return (board[index] == " " || board[index] == "" || board[index] == nil)? false : true
+end
+
